2932c7eb20e346dea5ba8c4679fb5f05cffdcc21
ttnord <143183896+ttnord@users.noreply.github.com>
2024-08-30 04:37:00 +0000

    some change

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang::tooling;
+using namespace llvm;
+
+enum class hello {
+a,b,c
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+}
